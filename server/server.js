7099fc2b687546da160eddf7bab98dba57944067
Justin Brewer <stan1651@gmail.com>
2021-11-26 19:04:01 -0800

    backend components

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");
+
+const{ typeDefs, resolvers } = require ("./schemas");
+const { authMiddleware } = require ("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,8 +26,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
